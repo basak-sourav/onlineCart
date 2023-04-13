1be9793e94301efaa27d87601a9935e3a60b8498
Sourav Basak <basak.rana84@gmail.com>
2023-04-13 18:20:41 +0530

    online cart

---
+++ src/Component/Cart.jsx
@@ -0,0 +1,52 @@
+import React, { createContext, useReducer, useEffect } from "react";
+import "../Styles/cart.css";
+import { products } from "./ProdApi";
+import ContextCart from "./ContextCart";
+import { Reducer } from "./Reducer";
+
+export const CartContext = createContext();
+
+const initialState = {
+  item: products,
+  totalAmount: 0,
+  totalItem: 0,
+};
+
+const Cart = () => {
+  const [state, dispatch] = useReducer(Reducer, initialState);
+  const removeItem = (id) => {
+    return dispatch({
+      type: "REMOVE_ITEM",
+      payload: id,
+    });
+  };
+
+  const clearCart = () => {
+    return dispatch({ type: "CLEAR_CART" });
+  };
+  const increment = (id) => {
+    return dispatch({
+      type: "INCREMENT",
+      payload: id,
+    });
+  };
+  const decrement = (id) => {
+    return dispatch({
+      type: "DECREMENT",
+      payload: id,
+    });
+  };
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOTAL" });
+  }, [state.item]);
+
+  return (
+    <CartContext.Provider
+      value={{ ...state, removeItem, clearCart, increment, decrement }}>
+      <ContextCart />
+    </CartContext.Provider>
+  );
+};
+
+export default Cart;
